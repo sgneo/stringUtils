5bd82b4881daa3d5234edc52d6659973b593de87
George Stan <sgneo93@gmail.com>
2019-12-22 14:05:01 +0200

    Adding trimStart and trimEnd

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 export const isEmpty = str => str.length === 0;
 export const isBlank = str => str.trim().length === 0;
 export const startsWith = (str1, str2) => str1.indexOf( str2 ) === 0;
+export const trimStart = str => str.replace(/^\s+/,'');
+export const trimEnd = str => str.replace(/\s+$/,'');
 
 
 export default {
   isEmpty,
   isBlank,
-  startsWith
+  startsWith,
+  trimStart,
+  trimEnd
 }
